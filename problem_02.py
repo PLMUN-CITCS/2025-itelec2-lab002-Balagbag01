c3ff37a0b14eda6da2ca3a5a2627ae003c82424b
Balagbag01 <andreiegualan1@gmail.com>
2025-02-13 22:05:58 +0800

    Update problem_02.py

--- problem_02.py
+++ problem_02.py
@@ -1,14 +1,14 @@
-# Egualan Andrei 
+# Egualan Andrei D. 
 # ITELEC2
 # Problem Set 01 - Problem 02
 # Square the Number Program
 
 def main():
-   print("Square the Number Program")
+    print("Square the Number Program")
 
-number = int(input("Enter a number: "))
+    number = int(input("Enter a number: "))
 
-print(f"The square of {number} is {(number * number):.2f}")
+    print(f"The square of {number} is {(number * number):.2f}")
 
 if __name__ == "__main__":
     main()
